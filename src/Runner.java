53899143866aa46d4562660175161e29b3f85f5e
Will Schiesswohl <william.schiesswohl@vtsd.com>
2015-11-26 10:54:34 -0500

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,14 +1,47 @@
+// Lab11avst.java
+// The "Sieve of Eratosthenes" Program
+// This is the student, starting version of the Lab11a assignment.
 
-public class Runner {
-	public static void main(String[] args) 
-	{
-		// This main method needs additions for the 100 point version.
-		Scanner input = new Scanner(System.in); 
-		final int MAX = 100;
-		boolean primes[];
-		primes = new boolean[MAX];
-		computePrimes(primes);
-		displayPrimes(primes);
-	}
 
+import java.util.Scanner;
+
+
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        // This main method needs additions for the 100 point version.
+        Scanner input = new Scanner(System.in);
+        final int MAX = 100;
+        boolean primes[];
+        primes = new boolean[MAX];
+        computePrimes(primes);
+        displayPrimes(primes);
+    }
+
+    public static void computePrimes(boolean primes[])
+    {
+        for(int k=0; k < 100; k++){
+            primes[k] = true;}
+        for(int p = 2; p < primes.length; p++){
+            for(int q = 2; q < primes.length; q++){
+                if(p*q < primes.length)
+                    primes[p*q] = false;
+            }
+        }
+    }
+
+    public static void displayPrimes(boolean primes[])
+    {
+        System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
+        System.out.println();
+        computePrimes(primes);
+        for(int k = 2; k < primes.length; k++){
+            if(primes[k] == true){
+                System.out.print(k + " ");}
+            //else if(primes[k] == false){
+            //    System.out.print("{" + k + "}");}
+        }
+
+    }
 }
