acbdd2a6d19221851143afcc5cae3ea4ddf58051
Will Schiesswohl <william.schiesswohl@vtsd.com>
2015-11-27 21:39:42 -0500

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -2,26 +2,26 @@
 // The "Sieve of Eratosthenes" Program
 // This is the student, starting version of the Lab11a assignment.
 
-
+//Works with user input but I could not get the decimal format to work
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
-
-public class Runner
+public class Lab11avst
 {
     public static void main(String[] args)
     {
         // This main method needs additions for the 100 point version.
         Scanner input = new Scanner(System.in);
-        final int MAX = 100;
-        boolean primes[];
-        primes = new boolean[MAX];
+        System.out.print("Enter the primes upper bound ==>>  ");
+        final int MAX = input.nextInt();
+        boolean primes[] = new boolean[MAX];
         computePrimes(primes);
         displayPrimes(primes);
     }
 
     public static void computePrimes(boolean primes[])
     {
-        for(int k=0; k < 100; k++){
+        for(int k=0; k < primes.length; k++){
             primes[k] = true;}
         for(int p = 2; p < primes.length; p++){
             for(int q = 2; q < primes.length; q++){
@@ -38,10 +38,8 @@ public class Runner
         computePrimes(primes);
         for(int k = 2; k < primes.length; k++){
             if(primes[k] == true){
-                System.out.print(k + " ");}
-            //else if(primes[k] == false){
-            //    System.out.print("{" + k + "}");}
+                System.out.print(k + " ");
+            }
         }
-
     }
 }
